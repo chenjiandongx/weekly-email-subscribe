668a50d2dca79603f5cded2fb03dcafd25abef2a
张天宇 <zhangty1996@163.com>
2021-01-03 10:26:57 +0800

    Update receivers.py (#37)
    * Update receivers.py
    
    * Update receivers.py
    
    Co-authored-by: dongdong <chenjiandongx@qq.com>

--- receivers.py
+++ receivers.py
@@ -26,7 +26,6 @@ MAIL_RECEIVER = [
     "2320874815@qq.com",
     "rockbee@gmail.com",
     "kang011503@163.com",
-    "neil.googl@gmail.com",
     "ukuq@qq.com",
     "ex985179996@outlook.com",
     "winglim@qq.com",
@@ -63,4 +62,5 @@ MAIL_RECEIVER = [
     "116190596@qq.com",
     "825503975@qq.com",
     "zhangliup1115@gmail.com",
+    "1379130580@qq.com",
 ]
